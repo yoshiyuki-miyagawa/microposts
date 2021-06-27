2073d2b3fac205380f0523d647b08a420048361b
yoshiyuki.miya <akayoshi3844@yahoo.ne.jp>
2021-06-27 14:19:49 +0000

    show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
   def index
+    @pagy, @users = pagy(User.order(id: :desc), items: 25)
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
