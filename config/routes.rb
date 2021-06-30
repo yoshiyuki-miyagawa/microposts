aaca4c285a15d5d29259976d67af4e3a5646a945
yoshiyuki.miya <akayoshi3844@yahoo.ne.jp>
2021-06-30 16:08:52 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
     root to: 'toppages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    
+    get 'login', to: 'sessions#new'
+    post 'login', to: 'sessions#create'
+    delete 'logout', to: 'sessions#destroy'
+    
     get 'signup', to: 'users#new'
-    resources :users, only: [:index, :show, :create]
+    resources :users, only: [:index, :show, :new, :create]
 end
