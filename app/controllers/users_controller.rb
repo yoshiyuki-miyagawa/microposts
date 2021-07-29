61b8de88d009231fb68764e6c7219a9346d10b2f
yoshiyuki.miya <akayoshi3844@yahoo.ne.jp>
2021-07-29 17:19:40 +0000

    timeline

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+ before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
+
   def index
     @pagy, @users = pagy(User.order(id: :desc), items: 25)
   end
@@ -16,18 +17,32 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    
+
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
       redirect_to @user
     else
-      flash.now[:danger] ='ユーザの登録に失敗しました。'
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
     end
   end
+
+  def followings
+    @user = User.find(params[:id])
+    @pagy, @followings = pagy(@user.followings)
+    counts(@user)
+  end
+
+  def followers
+    @user = User.find(params[:id])
+    @pagy, @followers = pagy(@user.followers)
+    counts(@user)
+  end
+
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
+
